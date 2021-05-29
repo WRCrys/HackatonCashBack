bfe36360a09ef85fdbb212ec224765c7eecbefa1
Crystyano Almeida <111809@pmenos.com.br>
2021-05-29 20:08:03 -0300

    Adding the notificator in controller main

--- src/HackatonCashBack.Api/Controllers/CashBackController.cs
+++ src/HackatonCashBack.Api/Controllers/CashBackController.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HackatonCashBack.Bussines.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HackatonCashBack.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CashBackController : ControllerBase
+    public class CashBackController : MainController
     {
+        private readonly INotificator _notificator;
+
+        public CashBackController(INotificator notificator) : base(notificator)
+        {
+            _notificator = notificator;
+        }
     }
 }
--- src/HackatonCashBack.Api/Controllers/MainController.cs
+++ src/HackatonCashBack.Api/Controllers/MainController.cs
@@ -1,14 +1,64 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HackatonCashBack.Bussines.Interfaces;
+using HackatonCashBack.Bussines.Notifications;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace HackatonCashBack.Api.Controllers
 {
     [ApiController]
     public abstract class MainController : ControllerBase
     {
+        private readonly INotificator _notificator;
+        
+        protected MainController(INotificator notificator)
+        {
+            _notificator = notificator;
+        }
+
+        protected bool ValidOperation()
+        {
+            return !_notificator.AnyNotification();
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (ValidOperation())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notificator.GetNotifications().Select(n => n.Message)
+            });
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid) NotifyErrorInvalidModel(modelState);
+            return CustomResponse();
+        }
+
+        protected void NotifyErrorInvalidModel(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var error in errors)
+            {
+                var errorMessage = error.Exception == null ? error.ErrorMessage : error.Exception.Message;
+                NotifyError(errorMessage);
+            }
+        }
+
+        protected void NotifyError(string errorMessage)
+        {
+            _notificator.Handle(new Notification(errorMessage));
+        }
     }
 }
